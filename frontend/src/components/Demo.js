3147d7eb43dd6e048e2573df08a362ab7ebd4ec5
Khanh Huy Nguyen <huy2272@gmail.com>
2024-01-27 17:25:16 -0500

    Fixed css for video and added image capture

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -1,10 +1,18 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './Demo.css'
 
 const Demo = () => {
     const videoRef = useRef(null)
+    const photoRef = useRef(null)
+
+    const [hasPhoto, setHasPhoto] = useState(false);
+
     const getVideo = () => {
-        navigator.mediaDevices.getUserMedia({ video: true })
+        navigator.mediaDevices.getUserMedia({
+            video: {
+                optional: [{ maxWidth: 400 }],
+            }
+        })
             .then((stream) => {
                 let video = videoRef.current;
                 video.srcObject = stream;
@@ -12,6 +20,26 @@ const Demo = () => {
             })
     }
 
+    const takePhoto = () => {
+        const width = 400;
+        const height = 400 / (16 / 9);
+
+        let video = videoRef.current;
+        let photo = photoRef.current;
+        photo.width = width;
+        photo.height = height;
+
+        let context = photo.getContext('2d');
+        context.drawImage(video, 0, 0, width, height);
+        setHasPhoto(true);
+    }
+
+    const clearPhoto = () => {
+        let photo = photoRef.current;
+        let context = photoRef.getContext('2d');
+        context.drawImage(0, 0, photo.width, photo.height);
+        setHasPhoto(false);
+    }
 
     return (
         <div className='demo'>
@@ -28,6 +56,15 @@ const Demo = () => {
                                 <p className='video-txt'>Video</p>
                                 <video ref={videoRef}></video>
                                 <button onClick={getVideo}>Start camera</button>
+                                <div className='divider'>
+                                    <p><span>Or</span></p>
+                                </div>
+                                <div>
+                                    <canvas ref={photoRef}></canvas>
+                                    {hasPhoto
+                                        ? <button onClick={clearPhoto}>Clear</button>
+                                        : <button onClick={takePhoto}>Take a photo</button>}
+                                </div>
                             </div>
                             <div className='form-footer'>
                                 <p>
